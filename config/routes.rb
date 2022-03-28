0aa8bc54112098a7123e29f8c84697beafb0de38
selik <mattbrydg@gmail.com>
2022-03-28 16:29:02 -0600

    Add API versioning through route constraint and namespaced controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+
+  # namespace the controllers without including the version number in the URI
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+  # Future versions go ABOVE the default as Rails cycles through the routes top down until matches? returns true
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
+  end
+
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
-  resources :todos do
-    resources :items
-  end
+
 end
